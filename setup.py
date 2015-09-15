f75a6e74e1d4e4f4792e018e31576437d14112e2
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-09-15 09:29:09 -0500

    save setup

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
+#scintResponseExtension = Extension("pysubevent/libscintresponse",
+#                                   sources=["pysubevent/scintresponse.cc"],
+#                                   include_dirs=["pysubevent"])
+cythonExtensions = cythonize([ Extension("pysubevent/cycfdiscriminator",
+                                         ["pysubevent/cycfdiscriminator.pyx","pysubevent/cfdiscriminator.cc"],
+                                         language="c++"),
+                               Extension("cysubeventdisc",
+                                         ["cysubeventdisc.pyx","pysubevent/scintresponse.cc"],
+                                         language='c++')] )
+print cythonExtensions
+
 setup(
     name='pysubevent',
-    ext_modules=cythonize(["cysubeventdisc.pyx","pysubevent/cycfdiscriminator.pyx"]),
+    #ext_modules=[scintResponseExtension]+cythonExtensions,
+    ext_modules=cythonExtensions,
     include_dirs=[numpy.get_include()],
     packages=['pysubevent'],
 )  
