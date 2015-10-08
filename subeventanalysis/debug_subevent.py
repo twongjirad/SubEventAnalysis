c9358eea2a6c5c799233b28cdd590c388e05f4f3
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2015-10-07 21:40:56 -0500

    added run script. also implemented hacky baseline fix

--- subeventanalysis/debug_subevent.py
+++ subeventanalysis/debug_subevent.py
@@ -1,7 +1,8 @@
 import os,sys
 import numpy as np
+from pysubevent.utils.pedestal import getpedestal
 
-vis = False
+vis = True
 
 if vis:
     try:
@@ -13,15 +14,44 @@ if vis:
         PYQTGRAPH = False
 
 def prepWaveforms( opdata ):
+    RC = 50000.0 # ns
     nsamples = opdata.getNBeamWinSamples()
     wfms = np.ones( (nsamples,32) )*2047.0
+    qs   = np.zeros( (nsamples,32) )
     for ch in range(0,32):
+        scale = 1.0
         if np.max( opdata.getData()[:,ch] )<4090:
             wfms[:,ch] = opdata.getData(slot=5)[:,ch]
         else:
-            wfms[:,ch] = opdata.getData(slot=6)[0,ch] + 10.0*( opdata.getData(slot=6)[:,ch] - opdata.getData(slot=6)[0,ch] )
-            opdata.getData(slot=5)[:,ch] = wfms[:,ch]
-    return wfms
+            print "swap HG ch",ch," with LG wfm"
+            lgwfm = opdata.getData(slot=6)[:,ch]
+            lgped = getpedestal( lgwfm, 20, 2.0 )
+            if lgped is None:
+                print "ch ",ch," LG has bad ped"
+                lgped = opdata.getData(slot=6)[0,ch]
+            wfms[:,ch] = lgwfm-lgped
+            print "lg ped=",lgped," wfm[0-10]=",np.mean( wfms[0:10,ch] )
+            wfms[:,ch] *= 10.0
+            scale = 10.0
+            #opdata.getData(slot=5)[:,ch] = wfms[:,ch] + lgped
+            #opdata.getPedestal(slot=5)[ch] = lgped # since we removed the pedestal already
+        ped = getpedestal( wfms[:,ch], 20, 2.0*scale )
+        if ped is not None:
+            print "ch ",ch," ped=",ped
+            wfms[:,ch] -= ped
+        else:
+            print "ch ",ch," has bad ped"
+            wfms[:,ch] -= 0.0
+        # calc undershoot
+        for i in range(1,len(qs[:,ch])):
+            #for j in range(i+1,np.minimum(i+1+200,len(qs[:,ch])) ):
+            q = 50.0*(wfms[i,ch]/RC) + qs[i-1,ch]*np.exp(-1.0*15.625/RC) # 10 is fudge factor!
+            qs[i,ch] = q
+            wfms[i,ch] += q
+        opdata.getData(slot=5)[:,ch] = wfms[:,ch]
+        opdata.getPedestal(slot=5)[ch] = 0.0
+            
+    return wfms,qs
             
 
 def makeFlashPlotItem( flash, seconfig ):
@@ -49,9 +79,9 @@ def test_getChannelFlashes( ch, opdata, seconfig, opdisplay=None ):
     """tests getChannelFlashesCPP"""
     from pysubevent.pysubevent.cysubeventdisc import getChannelFlashesCPP
     if opdisplay is not None:
+        opdisplay.gotoEvent( opdata.current_event )
         opdisplay.clearUserWaveformItem()
-    opdata.getEvent(8)
-    wfms = prepWaveforms( opdata )
+    wfms,qs = prepWaveforms( opdata )
 
     for ch in range(0,32):
         wfm = wfms[:,ch]
@@ -59,15 +89,29 @@ def test_getChannelFlashes( ch, opdata, seconfig, opdisplay=None ):
         print "channel=",ch,":  number of flashes=",len(flashes)
         if opdisplay is not None:
             print "visualize! ",flashes
-            postwfm -= poswfm[0]
+            #postwfm -= postwfm[0]
             for flash in flashes:
                 chsubevent = makeFlashPlotItem( flash, seconfig )
                 opdisplay.addUserWaveformItem( chsubevent, ch=ch)
             # plot postwfm
             plot_postwfm = pg.PlotCurveItem()
             x = np.linspace( 0,  seconfig.nspersample*len(postwfm), len(postwfm) )
-            plot_postwfm.setData( x=x, y=postwfm, pen=(0,255,0,255) )
+            plot_postwfm.setData( x=x, y=postwfm, pen=(0,255,0,100) )
             opdisplay.addUserWaveformItem( plot_postwfm, ch=ch )
+            # plot diff waveform
+            diffwfm = np.zeros( len(wfm),  dtype=np.float )
+            for i in range( seconfig.cfddelay, len(wfm) ):
+                diffwfm[i] = wfm[i]-wfm[i-seconfig.cfddelay]
+            
+            plot_diffwfm = pg.PlotCurveItem()
+            x = np.linspace( 0,  seconfig.nspersample*len(diffwfm), len(diffwfm) )
+            plot_diffwfm.setData( x=x, y=diffwfm, pen=(0,100,255,100) )
+            opdisplay.addUserWaveformItem( plot_diffwfm, ch=ch )
+        plot_qs = pg.PlotCurveItem()
+        x = np.linspace( 0, seconfig.nspersample*len(qs[:,ch]), len(qs[:,ch]) )
+        y = qs[:,ch]
+        plot_qs.setData( x=x, y=y, pen=(100,100,0,100) )
+        opdisplay.addUserWaveformItem( plot_qs, ch=ch )
             
     print flashes
 
@@ -75,14 +119,12 @@ from pysubevent.pysubevent.cysubeventdisc import pyWaveformData, pySubEventIO, p
 
 def test_runSubEventFinder( opdata, seconfig, filename, opdisplay=None ):
 
-    opdata.getEvent(8)
-    wfms = prepWaveforms( opdata )   # extract numpy arrays
+    wfms,qs = prepWaveforms( opdata )   # extract numpy arrays
     pywfms = pyWaveformData( wfms )  # package
     for i in range(0,wfms.shape[1]):
         print "ch ",i,": max=",np.max(wfms[:,i])
 
     subeventio = pySubEventIO( filename, 'w' )
-
     
     from pysubevent.pysubevent.cysubeventdisc import formSubEventsCPP
     import pysubevent.utils.pmtcalib as spe
@@ -107,8 +149,8 @@ if __name__ == "__main__":
     from pylard.pylardata.rawdigitsopdata import RawDigitsOpData
     fname = "../../data/pmtratedata/run2668_filterreconnect_rerun.root"
     opdata = RawDigitsOpData( fname )
-    ok = opdata.getNextEvent()
-
+    #ok = opdata.getNextEvent()
+    ok = opdata.getEvent(4)
     if vis:
         app = QtGui.QApplication([])
         opdisplay = OpDetDisplay( opdata )
@@ -120,7 +162,7 @@ if __name__ == "__main__":
     if ok:
         #test_findonesubevent( 0, opdata, config, opdisplay=opdisplay )
         #test_getChannelFlashes( 0, opdata, config, opdisplay=opdisplay )
-        test_runSubEventFinder( opdata, config, "test.root", opdisplay=opdisplay )
+        test_runSubEventFinder( opdata, config, "output_debug.root", opdisplay=opdisplay )
 
 
     if vis and ( (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION')):
---
+++ subeventanalysis/runSubEventFinder.py
@@ -0,0 +1,98 @@
+import os,sys
+import numpy as np
+from pysubevent.utils.pedestal import getpedestal
+
+def prepWaveforms( opdata ):
+    RC = 50000.0 # ns
+    nsamples = opdata.getNBeamWinSamples()
+    wfms = np.ones( (nsamples,32) )*2047.0
+    qs   = np.zeros( (nsamples,32) )
+    for ch in range(0,32):
+        scale = 1.0
+        if np.max( opdata.getData()[:,ch] )<4090:
+            wfms[:,ch] = opdata.getData(slot=5)[:,ch]
+        else:
+            print "swap HG ch",ch," with LG wfm"
+            lgwfm = opdata.getData(slot=6)[:,ch]
+            lgped = getpedestal( lgwfm, 20, 2.0 )
+            if lgped is None:
+                print "ch ",ch," LG has bad ped"
+                lgped = opdata.getData(slot=6)[0,ch]
+            wfms[:,ch] = lgwfm-lgped
+            print "lg ped=",lgped," wfm[0-10]=",np.mean( wfms[0:10,ch] )
+            wfms[:,ch] *= 10.0
+            scale = 10.0
+            #opdata.getData(slot=5)[:,ch] = wfms[:,ch] + lgped
+            #opdata.getPedestal(slot=5)[ch] = lgped # since we removed the pedestal already
+        ped = getpedestal( wfms[:,ch], 20, 2.0*scale )
+        if ped is not None:
+            print "ch ",ch," ped=",ped
+            wfms[:,ch] -= ped
+        else:
+            print "ch ",ch," has bad ped"
+            wfms[:,ch] -= 0.0
+        # calc undershoot
+        for i in range(1,len(qs[:,ch])):
+            #for j in range(i+1,np.minimum(i+1+200,len(qs[:,ch])) ):
+            q = 50.0*(wfms[i,ch]/RC) + qs[i-1,ch]*np.exp(-1.0*15.625/RC) # 10 is fudge factor!
+            qs[i,ch] = q
+            wfms[i,ch] += q
+        opdata.getData(slot=5)[:,ch] = wfms[:,ch]
+        opdata.getPedestal(slot=5)[ch] = 0.0
+            
+    return wfms
+
+
+def runSubEventFinder( config, input, outfilename ):
+    print "Opening ... ", input
+
+    # Load Data
+    from pylard.pylardata.rawdigitsopdata import RawDigitsOpData
+    opdata = RawDigitsOpData( input )
+    ok = opdata.getNextEvent()
+    
+    # Load Calibration
+    from pysubevent.pysubevent.cysubeventdisc import formSubEventsCPP
+    import pysubevent.utils.pmtcalib as spe
+    pmtspe = spe.getCalib( "../config/pmtcalib_20150930.json" )
+
+    # Setup Output
+    from pysubevent.pysubevent.cysubeventdisc import pyWaveformData, pySubEventIO, pySubEventList
+
+    subeventio = pySubEventIO( outfilename, 'w' )
+
+    nevents = 0
+    while ok:
+        wfms = prepWaveforms( opdata )   # extract numpy arrays
+        pywfms = pyWaveformData( wfms )  # packages
+        
+        subevents = formSubEventsCPP( pywfms, config, pmtspe )
+
+        subeventio.clearlist()
+        subeventio.eventid = opdata.current_event
+        subeventio.chmaxamp = np.max( wfms )
+        print "chmaxamp: ",subeventio.chmaxamp-2047.0
+        subeventio.transferSubEventList( subevents )
+        subeventio.fill()
+        nevents += 1
+        #raw_input()
+        #if nevents>=50:
+        #    break
+        ok = opdata.getNextEvent()
+
+
+    subeventio.write()
+
+
+if __name__ == "__main__":
+
+    # Load config
+    from pysubevent.pysubevent.cysubeventdisc import pySubEventModConfig
+    config = pySubEventModConfig( "discr1", "subevent.cfg" )
+
+    inputfile = sys.argv[1]
+    outfile   = sys.argv[2]
+    
+    print inputfile, outfile
+    
+    runSubEventFinder( config, inputfile, outfile )
