73661db262a8caa13d30cd25e9557a1847926963
Taritree Wongjirad <taritree@mit.edu>
2015-10-09 19:10:22 -0500

    merged setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ export SUBEVENTDATA=${VIRTUAL_ENV}/../config
 export SUBEVENTPATH=${VIRTUAL_ENV}/lib/python2.7/site-packages/pysubevent
 export LD_LIBRARY_PATH=${SUBEVENTHOME}:${SUBEVENTPATH}/cfdiscriminator:${SUBEVENTPATH}/subevent:${LD_LIBRARY_PATH}
 export DYLD_LIBRARY_PATH=${SUBEVENTPATH}/cfdiscriminator:${SUBEVENTPATH}/subevent:${DYLD_LIBRARY_PATH}
+#export PYTHONPATH=${VIRTUAL_ENV}/lib/python2.7/site-packages:${PYTHONPATH}
+#export LD_LIBRARY_PATH=${VIRTUAL_ENV}/../:${LD_LIBRARY_PATH}
